476765e2a6c742e34f0c3a30c9033e0daefb7e52
neta <netanyabomani@gmail.com>
2022-06-05 22:34:52 -0400

    update publication images

--- publications/publicationTemplates.js
+++ publications/publicationTemplates.js
@@ -13,6 +13,9 @@ const publicationDetailTemplate = Handlebars.compile(`
 {{#if img1}}<img id="publicationImg" src="asset/{{img1}}" alt="{{img1AltText}}"/>{{/if}}
 {{#if img2}}<img id="publicationImg" src="asset/{{img2}}" alt="{{img2AltText}}"/>{{/if}}
 {{#if img3}}<img id="publicationImg" src="asset/{{img3}}" alt="{{img3AltText}}"/>{{/if}}
+{{#if img4}}<img id="publicationImg" src="asset/{{img4}}" alt="{{img4AltText}}"/>{{/if}}
+{{#if img5}}<img id="publicationImg" src="asset/{{img5}}" alt="{{img5AltText}}"/>{{/if}}
+{{#if img6}}<img id="publicationImg" src="asset/{{img6}}" alt="{{img6AltText}}"/>{{/if}}
 </div>
 <ul class="publication-details">
     <li>Publication date: {{date}}</li>
@@ -21,7 +24,7 @@ const publicationDetailTemplate = Handlebars.compile(`
     <li>Category: {{category}}</li>
     <li>Price: {{price}}</li>
 </ul>
-<a class="button" href="{{purchaseLink}}">{{price}} on Haymarket Books</a> <br>
+<a class="button" href="{{purchaseLink}}">{{price}} via Haymarket Books</a> <br>
 <p id="description">
     {{description}}
 </p>
