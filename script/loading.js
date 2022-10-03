eff93c95086f3d0e1ec7ed91590b3e5e9e140312
neta <netanyabomani@gmail.com>
2022-10-02 20:39:37 -0400

    safari footer patch

--- script/loading.js
+++ script/loading.js
@@ -1,9 +1,9 @@
 let links = document.links;
 
 for (let i = 0, linksLength = links.length; i < linksLength; i += 1) {
-   if (links[i].hostname != window.location.hostname) {
-       links[i].target = '_blank';
-   } 
+    if (links[i].hostname != window.location.hostname) {
+        links[i].target = '_blank';
+    }
 }
 
 var sentences = [
@@ -11,48 +11,55 @@ var sentences = [
     'I am going to keep on stinging till I arouse the conscience of America.',
     'The name has come. Sojourner, that is it. Because I am going to travel up and down the country showing the people their sins and being a sign onto them.'
 ],
-            part,
-            i = 0,
-            offset = 0,
-            len = sentences.length,
-            forwards = true,
-            skip_count = 0,
-            skip_delay = 7,
-            speed = 25;
-        var wordflick = function () {
-            setInterval(function () {
-                if (forwards) {
-                    if (offset >= sentences[i].length) {
-                        ++skip_count;
-                        if (skip_count == skip_delay) {
-                            forwards = false;
-                            skip_count = 0;
-                        }
-                    }
+    part,
+    i = 0,
+    offset = 0,
+    len = sentences.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 7,
+    speed = 25;
+var wordflick = function () {
+    setInterval(function () {
+        if (forwards) {
+            if (offset >= sentences[i].length) {
+                ++skip_count;
+                if (skip_count == skip_delay) {
+                    forwards = false;
+                    skip_count = 0;
                 }
-                else {
-                    if (offset == 0) {
-                        forwards = true;
-                        i++;
-                        offset = 0;
-                        if (i >= len) {
-                            i = 0;
-                        }
-                    }
+            }
+        }
+        else {
+            if (offset == 0) {
+                forwards = true;
+                i++;
+                offset = 0;
+                if (i >= len) {
+                    i = 0;
                 }
-                part = sentences[i].substr(0, offset);
-                if (skip_count == 0) {
-                    if (forwards) {
-                        offset++;
-                    }
-                    else {
-                        offset--;
-                    }
-                }
-                $('.loading').text(part);
-            }, speed);
-        };
+            }
+        }
+        part = sentences[i].substr(0, offset);
+        if (skip_count == 0) {
+            if (forwards) {
+                offset++;
+            }
+            else {
+                offset--;
+            }
+        }
+        $('.loading').text(part);
+    }, speed);
+};
 
-        $(document).ready(function () {
-            wordflick();
-        });
+$(document).ready(function () {
+    wordflick();
+});
+
+function detectBrowser() {
+    if (navigator.userAgent.includes("Safari")) {
+        return "safari"
+    }
+}
+document.body.className = detectBrowser()
